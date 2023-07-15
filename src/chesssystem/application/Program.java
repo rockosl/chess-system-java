1d251b271348a0e8a4eb51e047437dbca030ffd2
Ricardo <rockobaschirotto@gmail.com>
2023-07-15 10:55:11 -0300

    created board and piece

--- src/chesssystem/application/Program.java
+++ src/chesssystem/application/Program.java
@@ -1,14 +1,12 @@
 package chesssystem.application;
 
-import chesssystem.boardgame.Position;
+import chesssystem.boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
-		
+		Board board = new Board(8, 8);
 
 	}
 
